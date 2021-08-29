a925975bd7048c1c4b878d127be984a0c277cae5
Gaurav <parmargaurav1234@gmail.com>
2021-08-29 09:45:47 +0530

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,40 @@
 import React from "react";
 import "../App.css";
-import logo from "../portfolio_logo.jpg"
+import logo from "../portfolio_logo.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container">
+
         <a className="navbar-brand" href="/"><img className="loga" src={logo} alt="logo" /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+         <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
         </button>
       
         <div className="collapse navbar-collapse pickRight" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home</a>
+              <a className="nav-link" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">Link</a>
+              <a className="nav-link" href="#">About Me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacts</a>
             </li>
            
           </ul>
         
+        </div>
         </div>
       </nav>
     )
