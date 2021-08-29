eb98d437e50d0b7eb8c9bf9ee3dafacd3a5816ca
Gaurav <parmargaurav1234@gmail.com>
2021-08-29 15:05:00 +0530

    header compleated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
-
+import Header from './components/Header';
+import Particles from 'react-particles-js';
 function App() {
   return (
     <div className="App">
+    <>
+    <Particles
+          className="particles-canvas"
+    params={
+      {
+        Particles: {
+          number: {
+            density:{
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: "star",
+            stroke: {
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+        }
+      }
+    }
+     />
      <Navbar />
+     <Header />
+     </>
     </div>
   );
 }
